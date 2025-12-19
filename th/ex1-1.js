3673c2e312ba72d3cf5fa1174fd52b4bcf5b4099
champ <champdrm@gmail.com>
2025-12-19 10:51:17 +0700

    feat: add answer ex1-2

--- th/ex1-1.js
+++ th/ex1-1.js
@@ -8,6 +8,36 @@
 
 // Start Coding Here
 
+// function calculate(num1, num2, callback) {
+//     return callback(num1, num2);
+// }
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//     if (num2 === 0) {
+//         return "Cannot divide by zero";
+//     } else {
+//         return num1 / num2;
+//     }
+// }
+
+const calculate = (num1, num2, callback) => callback(num1, num2);
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num2 === 0 ? "Cannot divide by zero" : num1/num2
+
 
 // เมื่อ function นี้ทำงานจะเห็นผลลัพธ์ดังนี้
 console.log(calculate(10, 5, add)); // 15
